f8702cc5979dcd3591c9a60ffb2f4477234ad2ef
David Arutiunian <arutiunian666@gmail.com>
2024-04-29 00:00:16 +0300

    feat: better git diffs

--- packages/zsh/aliases.zsh
+++ packages/zsh/aliases.zsh
@@ -1,8 +1,12 @@
 alias c="clear"
-alias t="exa -n --long --tree --level=1"
+alias t="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias lg="lazygit"
+alias man="batman"
+
 alias dev="cd ~/Developer"
+
 alias zj="zellij"
 alias tm="tmux"
+
 alias tms="tmux-sessionizer"
 alias zjs="zellij-sessionizer"
