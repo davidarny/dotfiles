828c3cdfcb8ffac54f572792d500ecd1a5b26f61
David Arutyunyan <david.arutyunyan.me@gmail.com>
2024-05-05 18:51:05 +0300

    chore: upgrade config structure and remove js

--- config/.config/zsh/aliases.zsh
+++ config/.config/zsh/aliases.zsh
@@ -2,6 +2,7 @@ alias c="clear"
 alias t="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias lg="lazygit"
 alias man="batman"
+alias npmung="npm list -g --no-unicode | awk '/\+--/ {print $2}' | fzf --multi | cut -d'@' -f1 | xargs npm un -g"
 
 alias dev="cd ~/Developer"
 
