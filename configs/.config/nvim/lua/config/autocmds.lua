d90cb80ecbbc7d12b7f213ec216c26bfd1909071
David Arutyunyan <david.arutyunyan.me@gmail.com>
2024-09-18 11:47:11 +0300

    feat: autoupdate

--- configs/.config/nvim/lua/config/autocmds.lua
+++ configs/.config/nvim/lua/config/autocmds.lua
@@ -21,3 +21,16 @@
 -- end
 --
 -- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = augroup("autoupdate"),
+  callback = function()
+    require("lazy").update({
+      show = false,
+    })
+  end,
+})
