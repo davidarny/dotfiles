97822e5b39f6ba088327289929774bc77c9b0819
David Arutyunyan <david.arutyunyan.me@gmail.com>
2024-10-08 09:59:53 +0300

    feat: supermaven

--- configs/.config/nvim/lua/plugins/supermaven-nvim.lua
+++ configs/.config/nvim/lua/plugins/supermaven-nvim.lua
@@ -1,9 +1,10 @@
 return {
   {
     "supermaven-inc/supermaven-nvim",
-    commit = "40bde487fe31723cdd180843b182f70c6a991226",
     config = function()
-      require("supermaven-nvim").setup({})
+      require("supermaven-nvim").setup({
+        log_level = "off",
+      })
     end,
   },
 }
