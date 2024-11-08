a04c8ed575cb36abe70ef8f176cd216d2e41e56c
David Arutyunyan <david.arutyunyan.me@gmail.com>
2024-11-08 15:32:44 +0300

    fix: minor issues

--- configs/.config/nvim/lua/plugins/oil.lua
+++ configs/.config/nvim/lua/plugins/oil.lua
@@ -9,7 +9,7 @@ return {
         win_options = { signcolumn = 'yes:2' },
       }
 
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+      vim.keymap.set('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open parent directory' })
     end,
   },
   {
