34ae3c3f1b5ceff68c36333b4fc9e8dd274c1f5c
David Arutyunyan <david.arutyunyan.me@gmail.com>
2024-11-18 18:50:56 +0300

    feat: add nix-darwin

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 stow --adopt --target="$HOME" .
-home-manager switch --flake ./nix
+home-manager switch --flake ./nix --impure --verbose
+darwin-rebuild switch --flake ./nix --impure --verbose
