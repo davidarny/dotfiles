56aacd5caa9c35ba064b64517382b46572d10dd2
David Arutiunian <arutiunian666@gmail.com>
2024-11-18 18:50:56 +0300

    feat: add nix-darwin

---
+++ nix/darwin.nix
@@ -0,0 +1,27 @@
+{ self, pkgs, ... }:
+{
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  #$ darwin-rebuild changelog
+  system.stateVersion = 5;
+
+  system.defaults = {
+    NSGlobalDomain.AppleICUForce24HourTime = true;
+    NSGlobalDomain.AppleShowAllExtensions = true;
+    loginwindow.GuestEnabled = false;
+    finder.FXPreferredViewStyle = "Nlsv";
+  };
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  system.activationScripts.postActivation.text = ''
+    # Create symlink for Caddyfile
+    sudo ln -sf /Users/david_arutiunian/.dotfiles/Caddyfile /opt/homebrew/etc/Caddyfile
+  '';
+}
--- nix/home.nix
+++ nix/home.nix
@@ -7,13 +7,9 @@
   home.packages = [
     pkgs.git
     pkgs.neovim
+    pkgs.ripgrep
     pkgs.nixfmt-rfc-style # nix code formatter
   ];
-
-  # home.file.".config" = { source = ../.config; recursive = true; };
-  # home.file.".git" = { source = ../.git; recursive = true; };
-  # home.file.".local" = { source = ../.local; recursive = true; };
-  # home.file.".ssh" = { source = ../.ssh; recursive = true; };
 
   xdg.enable = true;
   programs.home-manager.enable = true;
