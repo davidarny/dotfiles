2d3d1aad6d2a98c8850001c2f56ee2325f676e94
David Arutyunyan <david.arutyunyan.me@gmail.com>
2024-11-20 17:00:18 +0300

    feat: add snacks.nvim

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -43,13 +43,13 @@ return {
       local user = vim.env.USER or 'User'
       user = user:sub(1, 1):upper() .. user:sub(2)
       return {
+        chat_autocomplete = true,
         auto_insert_mode = true,
         show_help = true,
         question_header = '  ' .. user .. ' ',
         answer_header = '  Copilot ',
-        window = {
-          width = 0.4,
-        },
+        window = { width = 0.4 },
+
         selection = function(source)
           local select = require 'CopilotChat.select'
           return select.visual(source) or select.buffer(source)
@@ -114,7 +114,6 @@ return {
     },
     config = function(_, opts)
       local chat = require 'CopilotChat'
-      require('CopilotChat.integrations.cmp').setup()
 
       vim.api.nvim_create_autocmd('BufEnter', {
         pattern = 'copilot-chat',
