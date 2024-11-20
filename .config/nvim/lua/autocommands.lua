254a55249d82407dc3e20bad7a146ef7b156e8a1
David Arutiunian <arutiunian666@gmail.com>
2024-11-20 17:13:02 +0300

    fix: formatting with LSP

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -15,10 +15,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 local disabled_filetypes = { 'oil' }
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-  group = vim.api.nvim_create_augroup('kickstart-format-on-save', {}),
+  group = vim.api.nvim_create_augroup('kickstart-format-on-save', { clear = true }),
   callback = function()
+    -- Check if not in disabled filetypes
     if not vim.tbl_contains(disabled_filetypes, vim.bo.filetype) then
-      vim.lsp.buf.format()
+      -- Safely check for formatters
+      pcall(function()
+        -- Get list of available formatters
+        local clients = vim.lsp.get_active_clients { bufnr = 0 }
+
+        -- Check if any client has formatting capability
+        for _, client in ipairs(clients) do
+          if client.supports_method 'textDocument/formatting' then
+            vim.lsp.buf.format {
+              async = false,
+              filter = function(c)
+                return c.name == client.name
+              end,
+            }
+            break
+          end
+        end
+      end)
     end
   end,
 })
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,7 @@
 return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
-  event = 'VeryLazy',
+  lazy = false,
+  priority = 1000,
   config = function()
     -- Better Around/Inside textobjects
     --
