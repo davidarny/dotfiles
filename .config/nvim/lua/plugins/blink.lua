d6e97d91e0571a7ccd6900d772742064eb0e5f55
David Arutiunian <arutiunian666@gmail.com>
2024-12-19 15:11:48 +0300

    refactor: update Neovim plugin configurations for improved event handling and documentation
    - Added event triggers for the `blink.cmp` and `nvim-highlight-colors` plugins to enhance their loading behavior.
    - Improved documentation in the `tailwind.lua` file with additional comments and a GitHub link for the `nvim-highlight-colors` plugin.
    - These changes enhance the organization and maintainability of the Neovim setup.

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,5 +1,7 @@
 return {
   'saghen/blink.cmp',
+  event = { 'BufReadPre', 'BufNewFile' },
+
   -- optional: provides snippets for the snippet source
   dependencies = {
     'rafamadriz/friendly-snippets',
