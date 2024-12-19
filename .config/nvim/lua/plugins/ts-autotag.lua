2defc83ca35228312093e10a66c38a87b24cc42a
David Arutiunian <arutiunian666@gmail.com>
2024-12-19 13:37:04 +0300

    refactor: remove unused Neovim plugins and clean up configurations
    - Deleted configuration files for several unused plugins: `autopairs`, `highlight-colors`, `illuminate`, `incline`, `key-analyzer`, and `ts-autotag`.
    - Streamlined the `cmp.lua` plugin configuration by ensuring consistent formatting for completion sources.
    - This cleanup enhances the overall organization and maintainability of the Neovim setup.

--- .config/nvim/lua/plugins/ts-autotag.lua
+++
@@ -1,9 +0,0 @@
---- Automatically add closing tags for HTML and JSX.
----
---- See `:help nvim-ts-autotag` for more information.
---- GitHub: https://github.com/windwp/nvim-ts-autotag
-return {
-  'windwp/nvim-ts-autotag',
-  event = { 'BufReadPre', 'BufNewFile' },
-  opts = {},
-}
