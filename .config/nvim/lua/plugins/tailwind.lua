4e686b9021d1b504bb6bb956ceec119635ba1d88
David Arutiunian <arutiunian666@gmail.com>
2024-12-19 14:39:05 +0300

    refactor: remove cmp.lua and streamline plugin configurations
    - Deleted the `cmp.lua` configuration file, removing the associated completion plugin setup.
    - Updated the `image.lua` plugin configuration for improved readability.
    - Cleaned up the `lazydev.lua` configuration by adjusting the structure of plugin definitions.
    - Enhanced the `lint.lua` and `lsp.lua` configurations for better organization and clarity.
    - Reformatted key mappings in `snacks.lua` and `telescope.lua` for consistency.
    - Improved documentation for the `tailwind.lua` plugin with additional comments and GitHub links.
    - These changes enhance the maintainability and organization of the Neovim setup.

--- .config/nvim/lua/plugins/tailwind.lua
+++ .config/nvim/lua/plugins/tailwind.lua
@@ -1,8 +1,8 @@
---- A Neovim plugin to add vscode-style TailwindCSS completion to nvim-cmp.
----
---- See `:help tailwindcss-colorizer-cmp.nvim` for more information.
---- GitHub: https://github.com/roobert/tailwindcss-colorizer-cmp.nvim
 return {
+  --- A Neovim plugin to add vscode-style TailwindCSS completion to nvim-cmp.
+  ---
+  --- See `:help tailwindcss-colorizer-cmp.nvim` for more information.
+  --- GitHub: https://github.com/roobert/tailwindcss-colorizer-cmp.nvim
   {
     'roobert/tailwindcss-colorizer-cmp.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
@@ -13,6 +13,10 @@ return {
       }
     end,
   },
+  --- Highlights Tailwind CSS class names when @tailwindcss/language-server is connected.
+  ---
+  --- See `:help tailwindcss-colorizer-cmp.nvim` for more information.
+  --- GitHub: https://github.com/themaxmarchuk/tailwindcss-colors.nvim
   {
     'themaxmarchuk/tailwindcss-colors.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
