67343d661f3f7f95415c0450c5030d34a3081ba1
David Arutiunian <arutiunian666@gmail.com>
2024-12-20 16:50:15 +0300

    refactor: update key mappings in blink.lua and snacks.lua for improved usability
    - Changed the keymap preset in blink.lua from 'enter' to 'super-tab' for better functionality.
    - Streamlined key mappings in snacks.lua by removing unnecessary nested structures, enhancing readability and maintainability.
    - Added new key mappings for jumping to references and browsing Git, improving overall user experience.

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -23,7 +23,7 @@ return {
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
     -- your own keymap.
-    keymap = { preset = 'enter' },
+    keymap = { preset = 'super-tab' },
 
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
