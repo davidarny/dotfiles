ee02dfb1b1c34308a3a770b3ac09c639d74333fd
David Arutyunyan <david.arutyunyan.me@gmail.com>
2024-12-20 17:10:27 +0300

    docs: overhaul README.md and enhance Zsh configurations
    - Updated README.md to provide a comprehensive overview of included dotfiles, shell customization, prerequisites, and installation instructions.
    - Expanded shell aliases and functions in Zsh configuration files for improved terminal utility and navigation.
    - Added new aliases for terminal multiplexers and development tools, enhancing usability.
    - Introduced a function to migrate npm global packages to a new Node.js version, streamlining package management.

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,110 +1,24 @@
-# Run npm fzf
-function fnpm {
-    local packages
-    packages=$(all-the-package-names | fzf -m) &&
-    echo "$packages" &&
-    npm i $(echo $packages)
-}
-
-# Run git branch fzf
-function fbr {
-    local branches branch
-    branches=$(git branch --all | grep -v HEAD) &&
-    branch=$(echo "$branches" | 
-        fzf -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-    git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
-
-# Batch rename
-function brn {
-    for file in *$1*; do mv "$file" "${file/$1/$2}"; done
-}
-
-# Minify svg files in a folder
-function svgmin {
-    svgo --config /Users/david_arutiunian/svgo.config.js -f $1 -o $2
-}
-
-# Copy package version
-function cpver {
-    echo -n $(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g') | pbcopy
-}
-
-# Scaffold react component strucutre
-function crc {
-    mkdir $1 && cd $1
-
-    if [[ "$@" == *"-i"* ]]; then
-        cat << EOF >> index.ts
-export * from './${1}';
-EOF
-    fi
-
-    if [[ "$@" == *"-s"* ]]; then
-        cat << EOF >> $1.stories.tsx
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-
-import { ${1} } from './${1}';
-
-export default {
-  component: ${1},
-  title: 'components/${1}',
-} as ComponentMeta<typeof ${1}>;
-
-const Template: ComponentStory<typeof ${1}> = (args) => {
-  return (
-    <div className="story">
-      <h2 className="story-title">Default</h2>
-      <${1} {...args} />
-    </div>
-  );
-};
-
-export const Default = Template.bind({});
-Default.args = {};
-EOF
-    fi
-
-    cat << EOF >> $1.types.ts
-export interface $1Props {
-  className?: string;
-}
-EOF
-
-    cat << EOF >> $1.module.scss
-@import 'src/styles/services';
-EOF
-
-    cat << EOF >> $1.tsx
-import { FC } from 'react';
-
-import { ${1}Props } from './${1}.types';
-
-import styles from './${1}.module.scss';
-
-export const ${1}: FC<${1}Props> = ({ className }) => {
-  return <div className={cn(styles.root, className)} />;
-};
-EOF
-
-    cd ..
-}
-
-# Migrate npm global packages to new node version
-function fnm_upgrade {
+# Migrate npm global packages to a new Node.js version using fnm
+#
+# This function helps migrate all globally installed npm packages
+# from one Node.js version to another using fnm (Fast Node Manager).
+# It lists all global packages in the current version and reinstalls
+# them in the target version.
+#
+# Usage:
+#   nmg <target_version>
+#
+# Arguments:
+#   target_version - The Node.js version to migrate packages to (e.g., "18" or "20.0.0")
+#
+# Example:
+#   nmg 20
+#   # -> migrates all global packages to Node.js 20.x
+#
+#   nmg 18.15.0
+#   # -> migrates all global packages to Node.js 18.15.0
+function nmg {
   fnm exec --using=$1 npm ls --global --json \
     | jq -r '.dependencies | to_entries[] | .key+"@"+.value.version' \
     | xargs npm i -g
-}
-
-# Upgrade @mwl/core-lib package
-function mwl_core_upgrade() {
-  local new_version="$1"
-  sed -i '' "s|\"@mwl/core-lib\": \".*\",|\"@mwl/core-lib\": \"$new_version\",|" package.json
-}
-
-# Upgrade @mwl/ui package
-function mwl_ui_upgrade() {
-  local new_version="$1"
-  sed -i '' "s|\"@mwl/ui\": \".*\",|\"@mwl/ui\": \"$new_version\",|" package.json
 }
