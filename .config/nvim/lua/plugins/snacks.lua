8a4f390cce6ebeaf8f18a9a96c62da14a5d3bf0a
David Arutyunyan <david.arutyunyan.me@gmail.com>
2024-12-20 16:50:15 +0300

    refactor: update key mappings in blink.lua and snacks.lua for improved usability
    - Changed the keymap preset in blink.lua from 'enter' to 'super-tab' for better functionality.
    - Streamlined key mappings in snacks.lua by removing unnecessary nested structures, enhancing readability and maintainability.
    - Added new key mappings for jumping to references and browsing Git, improving overall user experience.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -21,47 +21,24 @@ return {
     dashboard = { enabled = true },
     styles = { enabled = true },
   },
+  -- stylua: ignore
   keys = {
     -- Buffer Management
-    {
-      '<leader>bd',
-      function()
-        Snacks.bufdelete()
-      end,
-      desc = 'Delete buffer',
-    },
-    {
-      '<leader>ba',
-      function()
-        Snacks.bufdelete.all()
-        require('oil').open()
-      end,
-      desc = 'Delete all buffers',
-    },
+    { '<leader>bd', function() Snacks.bufdelete() end, desc = 'Delete buffer' },
+    { '<leader>ba', function() Snacks.bufdelete.all() end, desc = 'Delete all buffers' },
 
     -- Terminal
-    {
-      '<c-/>',
-      function()
-        Snacks.terminal()
-      end,
-      desc = 'Toggle Terminal',
-    },
-    {
-      '<c-_>',
-      function()
-        Snacks.terminal()
-      end,
-      desc = 'which_key_ignore',
-    },
+    { '<c-/>', function() Snacks.terminal() end, desc = 'Toggle Terminal' },
+    { '<c-_>', function() Snacks.terminal() end, desc = 'which_key_ignore' },
 
     -- Rename
-    {
-      '<leader>cR',
-      function()
-        Snacks.rename.rename_file()
-      end,
-      desc = 'Rename File',
-    },
+    { '<leader>cR', function() Snacks.rename.rename_file() end, desc = 'Rename File' },
+
+    -- Words
+    { ']]', function() Snacks.words.jump(vim.v.count1) end, desc = 'Next Reference', mode = { 'n', 't' } },
+    { '[[', function() Snacks.words.jump(-vim.v.count1) end, desc = 'Prev Reference', mode = { 'n', 't' } },
+
+    -- Git Browse
+    { '<leader>gB', function() Snacks.gitbrowse() end, desc = 'Git Browse', mode = { 'n', 'v' } },
   },
 }
