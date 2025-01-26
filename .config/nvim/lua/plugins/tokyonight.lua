bb260eb4e936261f84079c4b5b33ec4900638784
David Arutiunian <arutiunian666@gmail.com>
2025-01-27 00:59:38 +0300

    feat: lazyvim updated

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,17 +1,4 @@
---- A colorscheme plugin for Neovim.
----
---- See `:help tokyonight.nvim` for more information.
---- GitHub: https://github.com/folke/tokyonight.nvim
 return {
-  'folke/tokyonight.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
+  "folke/tokyonight.nvim",
+  opts = { style = "night" },
 }
--- .config/nvim/lua/plugins/wakatime.lua
+++
@@ -1,8 +0,0 @@
---- Vim plugin for automatic time tracking and metrics generated from your programming activity.
----
---- See `:help vim-wakatime` for more information.
---- GitHub: https://github.com/wakatime/vim-wakatime
-return {
-  'wakatime/vim-wakatime',
-  lazy = false,
-}
