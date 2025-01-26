15367541e73e836a75284342d1bbfedb660a7ac0
David Arutyunyan <david.arutyunyan.me@gmail.com>
2025-01-27 00:59:38 +0300

    feat: lazyvim updated

--- .config/nvim/lua/plugins/abolish.lua
+++
@@ -1,9 +0,0 @@
--- Abolish is a plugin that provides a set of mappings to help you quickly replace
--- words with similar words.
---
--- See `:help vim-abolish` for more information.
--- GitHub: https://github.com/tpope/vim-abolish
-return {
-  'tpope/vim-abolish',
-  lazy = true,
-}
--- .config/nvim/lua/plugins/git-conflict.lua
+++
@@ -1,9 +0,0 @@
---- A plugin to visualise and resolve merge conflicts in Neovim.
----
---- See `:help git-conflict.nvim` for more information.
---- GitHub: https://github.com/akinsho/git-conflict.nvim
-return {
-  'akinsho/git-conflict.nvim',
-  config = true,
-  event = { 'BufReadPre', 'BufNewFile' },
-}
