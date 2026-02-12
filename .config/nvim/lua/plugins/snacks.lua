3fa78b7d42639b6b45be9568a6f8ac54a8c93d7d
waosdx <arutiunian666@gmail.com>
2026-02-12 11:30:33 +0300

    feat: show hidden files in snacks file picker

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -6,6 +6,9 @@ return {
     },
     picker = {
       sources = {
+        files = {
+          hidden = true,
+        },
         explorer = {
           hidden = true,
           ignored = true,
