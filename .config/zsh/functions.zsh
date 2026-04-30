a96b8b4028e628769cca41e8be9a6e16966a02b6
David Arutyunyan <david.arutyunyan.me@gmail.com>
2026-04-30 18:06:58 +0300

    fix: derive Brewfile path in brew wrapper

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,10 +1,12 @@
 # Auto-sync Brewfile after install/uninstall
+typeset -g _dotfiles_brewfile="${${(%):-%x}:A:h:h:h}/Brewfile"
+
 function brew() {
   command brew "$@"
   local brew_status=$?
 
   if (( brew_status == 0 )) && [[ "$1" =~ ^(install|uninstall|remove|rmtree)$ ]]; then
-    command brew bundle dump --file=~/.dotfiles/Brewfile --force --no-vscode --no-go --no-cargo
+    command brew bundle dump --file="$_dotfiles_brewfile" --force --no-vscode --no-go --no-cargo
   fi
 
   return $brew_status
