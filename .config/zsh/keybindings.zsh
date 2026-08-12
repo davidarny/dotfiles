9741c77ab4b0ef8476c0fc854e1d8e073a5122a0
David Arutyunyan <david.arutyunyan.me@gmail.com>
2026-08-12 23:48:53 +0700

    fix: restore macOS-style zsh navigation

---
+++ .config/zsh/keybindings.zsh
@@ -0,0 +1,37 @@
+# Match macOS text editing: Option+Arrow moves by word and Shift selects.
+_zle_select_left() {
+  (( REGION_ACTIVE )) || zle set-mark-command
+  zle backward-char
+}
+
+_zle_select_right() {
+  (( REGION_ACTIVE )) || zle set-mark-command
+  zle forward-char
+}
+
+_zle_select_word_left() {
+  (( REGION_ACTIVE )) || zle set-mark-command
+  zle backward-word
+}
+
+_zle_select_word_right() {
+  (( REGION_ACTIVE )) || zle set-mark-command
+  zle forward-word
+}
+
+zle -N _zle_select_left
+zle -N _zle_select_right
+zle -N _zle_select_word_left
+zle -N _zle_select_word_right
+
+bindkey '\e[1;2D' _zle_select_left
+bindkey '\e[1;2C' _zle_select_right
+bindkey '\e[1;4D' _zle_select_word_left
+bindkey '\e[1;4C' _zle_select_word_right
+bindkey '\e[1;5D' undefined-key
+bindkey '\e[1;5C' undefined-key
+
+# Edit the current command in $EDITOR with Ctrl-X.
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey -M emacs '^X' edit-command-line
--- .zshrc
+++ .zshrc
@@ -9,3 +9,4 @@ source ~/.config/zsh/fzf.zsh
 source ~/.config/zsh/aliases.zsh
 source ~/.config/zsh/functions.zsh
 source ~/.config/zsh/tools.zsh
+source ~/.config/zsh/keybindings.zsh
