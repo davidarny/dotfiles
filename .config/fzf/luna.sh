aea3b9e111e7826c31eed8a73588406d9e3ed03e
David Arutyunyan <david.arutyunyan.me@gmail.com>
2026-08-14 16:59:19 +0700

    chore: switch configs to Luna theme
    Adopt the Luna palette across bat, Ghostty, fzf, Lazygit, Neovim,
    Starship, and Yazi, adding the required theme definitions.

---
+++ .config/fzf/luna.sh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#1c1c1c,bg:#060606,spinner:#a8a8a8,hl:#c2916a \
+--color=fg:#e4e4e8,header:#c2916a,info:#8c9cb8,pointer:#a8a8a8 \
+--color=marker:#b09080,fg+:#c7c7c7,prompt:#75a1c7,hl+:#c2916a \
+--color=border:#060606 \
+--multi"
---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "WTFox/luna.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "luna" },
+  },
+}
